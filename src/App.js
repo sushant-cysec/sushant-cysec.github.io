b9f467eae7998b52b2cf4577f309de3669b3aec2
sushant sharma <rsushant280@gmail.com>
2022-04-02 14:53:56 +0530

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import Header from "./header.js"
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Route
+  ,Redirect,
+  Switch
 
-function App() {
+} from "react-router-dom";
+
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import Project from "./pages/Project" 
+import Navbar from "./Components/navbar"
+import Background from "./Components/Background"
+
+ 
+const App = () => {
   return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    
+    <Router>
+      
+      <Navbar/>
+      <main>
+      
+        
+       <Switch>
+       <Route path="/" exact>
+          <Home/>
+          <Background/>
+          </Route>
+        <Route path="/About" exact>
+          <About/>
+          <Background/>
+          </Route>
+        <Route path="/Contact" exact>
+          <Contact/>
+          <Background/>
+        </Route>
+        <Route path="/Project" exact>
+          <Project/>
+          <Background/>
+        </Route>
+
+        <Redirect to="/"/>
+        </Switch>
+        
+        
+      </main>
+    </Router>
+  )
 }
 
-export default App;
+export default App
